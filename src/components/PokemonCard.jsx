dfdaf7815c8b765275370249081eae9ba0933a89
BAJRAMI FLAKRON <flakronbajrami10@gmail.com>
2024-03-26 11:45:49 +0100

    pokedex

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PokemonCard(){
+    return (
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        <figcaption>Bulbasaur</figcaption>
+    </figure>
+    );
+}
+
+export default PokemonCard;
